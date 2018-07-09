adfcb03ea653c556af4969b86f7910a0fb851ed4
fanhl <fanhonglin@klicen.com>
2018-07-09 17:52:15 +0800

    3d model

--- app/src/main/java/com/fanhl/rajawalidemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/rajawalidemo/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.fanhl.rajawalidemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import org.rajawali3d.view.ISurface
+import org.rajawali3d.view.SurfaceView
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val surface = SurfaceView(this)
+        surface.setFrameRate(60.0)
+        surface.renderMode = ISurface.RENDERMODE_WHEN_DIRTY
+
+        // Add mSurface to your root view
+        addContentView(surface, ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT))
+
+        val renderer = Renderer(this)
+        surface.setSurfaceRenderer(renderer)
     }
 }
