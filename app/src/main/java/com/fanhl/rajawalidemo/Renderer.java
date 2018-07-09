30fc838fc598de1a2dcedfbf2f93bd563fdd161f
fanhl <fanhonglin@klicen.com>
2018-07-09 17:50:02 +0800

    Renderer

--- app/src/main/java/com/fanhl/rajawalidemo/Renderer.java
+++ app/src/main/java/com/fanhl/rajawalidemo/Renderer.java
@@ -1,9 +1,15 @@
 package com.fanhl.rajawalidemo;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import org.rajawali3d.lights.DirectionalLight;
+import org.rajawali3d.materials.Material;
+import org.rajawali3d.materials.methods.DiffuseMethod;
+import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.Texture;
+import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.primitives.Sphere;
 
 public class Renderer extends org.rajawali3d.renderer.Renderer {
@@ -20,7 +26,33 @@ public class Renderer extends org.rajawali3d.renderer.Renderer {
 
     @Override
     protected void initScene() {
+        directionalLight = new DirectionalLight(1f, .2f, -1.0f);
+        directionalLight.setColor(1.0f, 1.0f, 1.0f);
+        directionalLight.setPower(2);
+        getCurrentScene().addLight(directionalLight);
 
+        Material material = new Material();
+        material.enableLighting(true);
+        material.setDiffuseMethod(new DiffuseMethod.Lambert());
+        material.setColor(0);
+
+        Texture earthTexture = new Texture("Earth", R.drawable.texture_head);
+        try {
+            material.addTexture(earthTexture);
+        } catch (ATexture.TextureException error) {
+            Log.d("DEBUG", "TEXTURE ERROR");
+        }
+
+        earthSphere = new Sphere(1, 24, 24);
+        earthSphere.setMaterial(material);
+        getCurrentScene().addChild(earthSphere);
+        getCurrentCamera().setZ(4.2f);
+    }
+
+    @Override
+    protected void onRender(long ellapsedRealtime, double deltaTime) {
+        super.onRender(ellapsedRealtime, deltaTime);
+        earthSphere.rotate(Vector3.Axis.Y, 1.0);
     }
 
     @Override
